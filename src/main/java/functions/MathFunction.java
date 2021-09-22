c144a98b9efcf21ddad594d99f754d96e492572a
hovanoff <hovidze93@mail.com>
2021-09-22 10:57:33 +0400

    Функция "а потом"

--- src/main/java/functions/MathFunction.java
+++ src/main/java/functions/MathFunction.java
@@ -2,7 +2,5 @@ package functions;
 
 public interface MathFunction {
     double apply(double x);
-    default CompositeFunction andThen(MathFunction afterFunction) {
-        return new CompositeFunction(this, afterFunction);
-    }
+    default CompositeFunction andThen(MathFunction afterFunction) { return new CompositeFunction(this, afterFunction); }
 }
