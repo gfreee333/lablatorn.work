3e33396a9a978a648e8c07c143797e30325b6c9d
hovanoff <hovidze93@mail.com>
2021-09-22 18:56:08 +0400

    исправление теста для "andThen()"

--- src/test/java/ru/ssau/tk/ivan/lablatorn/work/MathFunctionTest.java
+++ src/test/java/ru/ssau/tk/ivan/lablatorn/work/MathFunctionTest.java
@@ -5,15 +5,18 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class MathFunctionTest {
-    MathFunction first = new ConstantFunction(5);
-    MathFunction second = new SqrFunction();
-    private final MathFunction firstFunction = first.andThen(second);
-    private final MathFunction secondFunction = second.andThen(first);
+    MathFunction first = new ConstantFunction(5.0);
+    MathFunction sqr = new SqrFunction();
+    MathFunction tang = new TangFunction();
+    MathFunction root = new ThreeRootFunction();
+    private final MathFunction firstFunction = first.andThen(sqr);
+    private final MathFunction secondFunction = sqr.andThen(first);
 
     @Test
     public void andThen() {
-        assertEquals(firstFunction.apply(5), 25, 0.00001);
-        assertEquals(secondFunction.apply(5), 5, 0.00001);
-        assertEquals(firstFunction.apply(-4), 25, 0.00001);
+        assertEquals(firstFunction.apply(5.0), 25.0, 0.00001);
+        assertEquals(secondFunction.apply(5.0), 5.0, 0.00001);
+        assertEquals(firstFunction.apply(-4.0), 25.0, 0.00001);
+        assertEquals(tang.andThen(sqr).andThen(root).apply(45.0), 1.3791, 0.0001);
     }
 }
