7a8d504b029bb7bf870bc0f6dd64d65b9b8ca8ba
ЧухмановИван <oollvanolloo@yandex.ru>
2021-09-26 18:22:53 +0400

    2.2 создан интерфейс TabulatedFunction

--- src/main/java/ru/ssau/tk/ivan/lablatorn/work/TabulatedFunction.java
+++ src/main/java/ru/ssau/tk/ivan/lablatorn/work/TabulatedFunction.java
@@ -2,12 +2,18 @@ package ru.ssau.tk.ivan.lablatorn.work;
 
 public interface TabulatedFunction {
     int getCount();
-    double getX(int Index);
-    double getY(int index);
-    void setY(int index, double value);
-    int indexOfX(double x);
-    int indexOfY(double y);
-    double leftBound();
-    double rigthBound();
 
+    double getX(int Index);
+
+    double getY(int index);
+
+    void setY(int index, double value);
+
+    int indexOfX(double x);
+
+    int indexOfY(double y);
+
+    double leftBound();
+
+    double rigthBound();
 }
