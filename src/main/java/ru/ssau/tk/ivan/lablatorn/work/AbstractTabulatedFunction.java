33ba12879dd2764a2572b9778b31e9be33235f72
hovanoff <hovidze93@mail.com>
2021-10-04 03:19:34 +0400

    Абстрактный TabulatedFunction

--- src/main/java/ru/ssau/tk/ivan/lablatorn/work/AbstractTabulatedFunction.java
+++ src/main/java/ru/ssau/tk/ivan/lablatorn/work/AbstractTabulatedFunction.java
@@ -1,5 +1,25 @@
 package ru.ssau.tk.ivan.lablatorn.work;
 
-public class AbstractTabulatedFunction {
+public abstract class AbstractTabulatedFunction implements TabulatedFunction {
+
+    protected abstract int floorIndexOfX(double x);
+
+    protected abstract double extrapolateLeft(double x);
+
+    protected abstract double extrapolateRight(double x);
+
+    protected abstract double interpolate(double x, int floorIndex);
+
+    protected double interpolate(double x, double leftX, double rightX, double leftY, double rightY) {
+        return leftY + (rightY - leftY) * (x - leftX) / (rightX - leftX);
+    }
+
+    @Override
+    public double apply(double x) {
+        if (x < leftBound()) return extrapolateLeft(x);
+        if (x > rightBound()) return extrapolateRight(x);
+        if (indexOfX(x) != -1) return getY(indexOfX(x));
+        return interpolate(x, floorIndexOfX(x));
+    }
 
 }
