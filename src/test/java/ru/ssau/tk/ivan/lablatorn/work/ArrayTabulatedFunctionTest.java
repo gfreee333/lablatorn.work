cc9f97e0a7dc7f71045000fb316797e0aeaca4e1
Чухманов Иван Викторович <oollvanolloo@yandex.ru>
2021-10-14 01:24:19 +0400

    исправил тесты пункт 5 для ArrayTabulatedFunctionTest

--- src/test/java/ru/ssau/tk/ivan/lablatorn/work/ArrayTabulatedFunctionTest.java
+++ src/test/java/ru/ssau/tk/ivan/lablatorn/work/ArrayTabulatedFunctionTest.java
@@ -2,7 +2,6 @@ package ru.ssau.tk.ivan.lablatorn.work;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
 import static org.testng.Assert.assertEquals;
 
 public class ArrayTabulatedFunctionTest {
@@ -12,116 +11,139 @@ public class ArrayTabulatedFunctionTest {
     private final double END = 101;
     private final double[] xValues = new double[]{3.4, 5.2, 6.0, 2.1};
     private final double[] yValues = new double[]{-2.4, 1.2, 3.0, 5.1};
-    private final double[] xValuesOne = new double[]{34, 5.2, 60, 2};
-    private final double[] yValuesOne = new double[]{-2.4, 1.2, 30, 5.1};
-    private final double[] xValuesThree = new double[]{3.0};
-    private final double[] yValuesThree = new double[]{4.0};
 
-    private final SqrFunction sqrObject = new SqrFunction();
-    private final ArrayTabulatedFunction arrayTabulatedObject = new ArrayTabulatedFunction(xValues, yValues);
-    private final ArrayTabulatedFunction arrayTabulatedObjectOne = new ArrayTabulatedFunction(xValuesOne, yValuesOne);
-    private final ArrayTabulatedFunction arrayTabulatedObjectTwo = new ArrayTabulatedFunction(sqrObject, BEGIN, END, 100);
-    private final ArrayTabulatedFunction arrayTabulatedObjectWithOneElement = new ArrayTabulatedFunction(xValuesThree, yValuesThree);
+    private final MathFunction sqr = new SqrFunction();
+
+    private ArrayTabulatedFunction createFromArray() {
+        return new ArrayTabulatedFunction(xValues, yValues);
+    }
+
+
+    private ArrayTabulatedFunction firstFunction() {
+        return new ArrayTabulatedFunction(sqr, BEGIN, END, 100);
+    }
 
 
     @Test
     public void testSetY() {
-        arrayTabulatedObjectOne.setY(2, 1000);
-        assertEquals(arrayTabulatedObjectOne.getY(2), 1000, 0.001);
-        arrayTabulatedObjectOne.setY(2, 2222);
-        assertEquals(arrayTabulatedObjectOne.getY(2), 2222, 0.001);
+        ArrayTabulatedFunction array = createFromArray();
+        array.setY(2, 1000);
+        assertEquals(array.getY(2), 1000, DELTA);
+        array.setY(2, 2222);
+        assertEquals(array.getY(2), 2222, DELTA);
     }
 
     @Test
     public void testArrayTabulatedFunctionWithTwoParameters() {
-        assertEquals(arrayTabulatedObject.getCount(), 4);
-
+        ArrayTabulatedFunction array = createFromArray();
+        ArrayTabulatedFunction firstFunction = firstFunction();
+        assertEquals(array.getCount(), 4);
+        assertEquals(firstFunction.getCount(), 100);
     }
 
     @Test
     public void testGetCount() {
-        assertEquals(arrayTabulatedObjectTwo.getCount(), 100);
-        assertEquals(arrayTabulatedObjectWithOneElement.getCount(), 1);
+        ArrayTabulatedFunction array = createFromArray();
+        ArrayTabulatedFunction firstFunction = firstFunction();
+        assertEquals(firstFunction.getCount(), 100);
+        assertEquals(array.getCount(), 4);
     }
 
     @Test
     public void testGetX() {
+        ArrayTabulatedFunction firstFunction = firstFunction();
         for (int element = 0; element < 99; element++) {
-            assertEquals(arrayTabulatedObjectTwo.getX(element), element * (END - BEGIN) / 100.0 + BEGIN, DELTA);
+            assertEquals(firstFunction.getX(element), element * (END - BEGIN) / 100.0 + BEGIN, DELTA);
         }
     }
 
     @Test
     public void testGetY() {
+        ArrayTabulatedFunction firstFunction = firstFunction();
         for (int element = 0; element < 99; element++) {
-            assertEquals(arrayTabulatedObjectTwo.getY(element), sqrObject.apply(arrayTabulatedObjectTwo.getX(element)), DELTA);
+            assertEquals(firstFunction.getY(element), sqr.apply(firstFunction.getX(element)), DELTA);
         }
     }
 
     @Test
     public void testLeftBound() {
-        assertEquals(arrayTabulatedObjectTwo.leftBound(), BEGIN, DELTA);
-        assertEquals(arrayTabulatedObjectWithOneElement.leftBound(), 3.0, DELTA);
+        ArrayTabulatedFunction firstFunction = firstFunction();
+        ArrayTabulatedFunction array = createFromArray();
+        assertEquals(firstFunction.leftBound(), BEGIN, DELTA);
+        assertEquals(array.leftBound(), 3.4, DELTA);
     }
 
     @Test
     public void testRightBound() {
-        assertEquals(arrayTabulatedObjectTwo.rightBound(), END, DELTA);
-        assertEquals(arrayTabulatedObjectWithOneElement.rightBound(), 3.0, DELTA);
+        ArrayTabulatedFunction firstFunction = firstFunction();
+        ArrayTabulatedFunction array = createFromArray();
+        assertEquals(firstFunction.rightBound(), END, DELTA);
+        assertEquals(array.rightBound(), 2.1, DELTA);
     }
 
     @Test
     public void testIndexOfX() {
-        assertEquals(arrayTabulatedObjectTwo.indexOfX(30.6), -1, DELTA);
-        assertEquals(arrayTabulatedObjectTwo.indexOfX(5), 4, DELTA);
+        ArrayTabulatedFunction firstFunction = firstFunction();
+        ArrayTabulatedFunction array = createFromArray();
+        assertEquals(firstFunction.indexOfX(30.6), -1, DELTA);
+        assertEquals(firstFunction.indexOfX(5), 4, DELTA);
         for (int element = 0; element < 99; ++element) {
-            assertEquals(arrayTabulatedObjectTwo.indexOfX(1 + element * (END - BEGIN) / 100.0), element);
+            assertEquals(firstFunction.indexOfX(1 + element * (END - BEGIN) / 100.0), element);
         }
-        assertEquals(arrayTabulatedObjectWithOneElement.indexOfX(3.0), 0, DELTA);
+        assertEquals(array.indexOfX(3), -1, DELTA);
     }
 
     @Test
     public void testIndexOfY() {
-        assertEquals(arrayTabulatedObjectTwo.indexOfY(0.432), -1, DELTA);
+        ArrayTabulatedFunction firstFunction = firstFunction();
+        ArrayTabulatedFunction array = createFromArray();
+        assertEquals(firstFunction.indexOfY(0.432), -1, DELTA);
         for (int element = 0; element < 99; element++) {
-            assertEquals(arrayTabulatedObjectTwo.indexOfY(sqrObject.apply(arrayTabulatedObjectTwo.getX(element))), element);
+            assertEquals(firstFunction.indexOfY(sqr.apply(firstFunction.getX(element))), element);
         }
-        assertEquals(arrayTabulatedObjectWithOneElement.indexOfY(4.0), 0);
+        assertEquals(array.indexOfY(3), 2);
     }
 
     @Test
     public void testFloorIndexOfX() {
+        ArrayTabulatedFunction firstFunction = firstFunction();
+        ArrayTabulatedFunction array = createFromArray();
         for (int element = 0; element < 99; element++) {
-            assertEquals(arrayTabulatedObjectTwo.floorIndexOfX(BEGIN + element * (END - BEGIN) / 100.0), element, DELTA);
+            assertEquals(firstFunction.floorIndexOfX(BEGIN + element * (END - BEGIN) / 100.0), element, DELTA);
         }
-        assertEquals(arrayTabulatedObjectTwo.floorIndexOfX(0.1), 0);
-        assertEquals(arrayTabulatedObjectTwo.floorIndexOfX(102.0), 100);
-        assertEquals(arrayTabulatedObjectTwo.floorIndexOfX(30.6), 29);
-        assertEquals(arrayTabulatedObjectWithOneElement.floorIndexOfX(3.0), 0);
+        assertEquals(firstFunction.floorIndexOfX(0.1), 0);
+        assertEquals(firstFunction.floorIndexOfX(102.0), 100);
+        assertEquals(firstFunction.floorIndexOfX(30.6), 29);
+        assertEquals(array.floorIndexOfX(3.0), 0);
     }
 
     @Test
     public void testExtrapolateLeft() {
-        assertEquals(arrayTabulatedObjectTwo.extrapolateLeft(0.1), -1.7000, DELTA);
-        assertEquals(arrayTabulatedObjectTwo.extrapolateLeft(0.9), 0.70000, DELTA);
-        assertEquals(arrayTabulatedObjectTwo.extrapolateLeft(Double.NEGATIVE_INFINITY), Double.NEGATIVE_INFINITY);
-        assertEquals(arrayTabulatedObjectWithOneElement.extrapolateLeft(0.1), 4.0, DELTA);
+        ArrayTabulatedFunction firstFunction = firstFunction();
+        ArrayTabulatedFunction array = createFromArray();
+        assertEquals(firstFunction.extrapolateLeft(0.1), -1.7000, DELTA);
+        assertEquals(firstFunction.extrapolateLeft(0.9), 0.70000, DELTA);
+        assertEquals(firstFunction.extrapolateLeft(Double.NEGATIVE_INFINITY), Double.NEGATIVE_INFINITY);
+        assertEquals(array.extrapolateLeft(0.1),-8.9999, DELTA);
     }
 
     @Test
     public void testExtrapolateRight() {
-        assertEquals(arrayTabulatedObjectTwo.extrapolateRight(100), 10001.0, DELTA);
-        assertEquals(arrayTabulatedObjectTwo.extrapolateRight(120), 14001.0, DELTA);
-        assertEquals(arrayTabulatedObjectTwo.extrapolateRight(Double.POSITIVE_INFINITY), Double.POSITIVE_INFINITY);
-        assertEquals(arrayTabulatedObjectWithOneElement.extrapolateRight(4.0), 4.0, DELTA);
+        ArrayTabulatedFunction firstFunction = firstFunction();
+        ArrayTabulatedFunction array = createFromArray();
+        assertEquals(firstFunction.extrapolateRight(100), 10001.0, DELTA);
+        assertEquals(firstFunction.extrapolateRight(120), 14001.0, DELTA);
+        assertEquals(firstFunction.extrapolateRight(Double.POSITIVE_INFINITY), Double.POSITIVE_INFINITY);
+        assertEquals(array.extrapolateRight(4.0), 4.0769, DELTA);
     }
 
     @Test
     public void testApply() {
-        assertEquals(arrayTabulatedObjectTwo.apply(0.6), -0.2000, DELTA);
-        assertEquals(arrayTabulatedObjectTwo.apply(110), 12001.0, DELTA);
-        assertEquals(arrayTabulatedObjectTwo.apply(50), 2500.0, DELTA);
-        assertEquals(arrayTabulatedObjectTwo.apply(30.6), 936.6, DELTA);
+        ArrayTabulatedFunction firstFunction = firstFunction();
+        assertEquals(firstFunction.apply(0.6), -0.2000, DELTA);
+        assertEquals(firstFunction.apply(110), 12001.0, DELTA);
+        assertEquals(firstFunction.apply(50), 2500.0, DELTA);
+        assertEquals(firstFunction.apply(30.6), 936.6, DELTA);
     }
 }
 
