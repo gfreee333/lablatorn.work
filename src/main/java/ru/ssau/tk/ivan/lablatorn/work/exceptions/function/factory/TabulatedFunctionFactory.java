8489654cbd3d6abe8039e8ffe246e465aed92225
Чухманов Иван Викторович <oollvanolloo@yandex.ru>
2021-10-25 14:53:54 +0400

    Изменил названия + постепенно начинаю решать проблему с модификаторами доступа. ( осталось доделать эти тесты на разных фабриках )

--- src/main/java/ru/ssau/tk/ivan/lablatorn/work/exceptions/function/factory/TabulatedFunctionFactory.java
+++
@@ -1,7 +0,0 @@
-package ru.ssau.tk.ivan.lablatorn.work.exceptions.function.factory;
-
-import ru.ssau.tk.ivan.lablatorn.work.function.TabulatedFunction;
-
-public interface TabulatedFunctionFactory {
-    TabulatedFunction create(double[] xValues, double[] yValues);
-}
--- src/test/java/ru/ssau/tk/ivan/lablatorn/work/operations/TabulatedFunctionOperationServiceTest.java
+++ src/test/java/ru/ssau/tk/ivan/lablatorn/work/operations/TabulatedFunctionOperationServiceTest.java
@@ -8,23 +8,23 @@ import ru.ssau.tk.ivan.lablatorn.work.function.Point;
 
 import static org.testng.Assert.*;
 
-public class TabulatedFunctionOperationServiceTest {
+public class TabulatedFunctionOperationServiceTest { // Доделать эти тесты относительно двух фабрик. вывести их как поля в начале.
     private final double[] valuesX = new double[]{-5, -4, -3, 6, 8, 10, 27};
     private final double[] valuesY = new double[]{-33, -22, -1, 0, 21, 25, 30};
     private final double[] valuesYForList = new double[]{1, 2, 3, 4, 5, 6, 7};
-    double DELTA = 0.001;
+    private final double DELTA = 0.001;
 
-    ArrayTabulatedFunction getTestArray() {
+    private ArrayTabulatedFunction getTestArray() {
         return new ArrayTabulatedFunction(valuesX, valuesY);
     }
 
-    LinkedListTabulatedFunction getTestList() {
+    private LinkedListTabulatedFunction getTestList() {
         return new LinkedListTabulatedFunction(valuesX, valuesYForList);
     }
 
     @Test
     public void testAsPoints() {
-        ArrayTabulatedFunction testArrayFunction = getTestArray();
+        TabulatedFunction testArrayFunction = getTestArray();
         Point[] Points = TabulatedFunctionOperationService.asPoints(testArrayFunction);
         int i = 0;
         for (Point myPoint : Points) {
@@ -32,7 +32,7 @@ public class TabulatedFunctionOperationServiceTest {
             assertEquals(myPoint.y, testArrayFunction.getY(i++), DELTA);
         }
         assertEquals(testArrayFunction.getCount(), i);
-        LinkedListTabulatedFunction testListFunction = getTestList();
+        TabulatedFunction testListFunction = getTestList();
         Points = TabulatedFunctionOperationService.asPoints(testListFunction);
         i = 0;
         for (Point myPoint : Points) {
@@ -44,8 +44,8 @@ public class TabulatedFunctionOperationServiceTest {
 
     @Test
     public void testSum() {
-        ArrayTabulatedFunction testArrayFunction = getTestArray();
-        LinkedListTabulatedFunction testListFunction = getTestList();
+        TabulatedFunction testArrayFunction = getTestArray();
+        TabulatedFunction testListFunction = getTestList();
 
         TabulatedFunction testSumOfArrays = new TabulatedFunctionOperationService().sum(testArrayFunction, testArrayFunction);
         int i = 0;
@@ -72,8 +72,8 @@ public class TabulatedFunctionOperationServiceTest {
 
     @Test
     public void testSubtract() {
-        ArrayTabulatedFunction testArrayFunction = getTestArray();
-        LinkedListTabulatedFunction testListFunction = getTestList();
+        TabulatedFunction testArrayFunction = getTestArray();
+        TabulatedFunction testListFunction = getTestList();
 
         TabulatedFunction testSubtractOfArrays = new TabulatedFunctionOperationService().subtract(testArrayFunction, testArrayFunction);
         int i = 0;
@@ -99,8 +99,8 @@ public class TabulatedFunctionOperationServiceTest {
 
     @Test
     public void testMultiplication() {
-        ArrayTabulatedFunction testArrayFunction = getTestArray();
-        LinkedListTabulatedFunction testListFunction = getTestList();
+        TabulatedFunction testArrayFunction = getTestArray();
+        TabulatedFunction testListFunction = getTestList();
 
         TabulatedFunction testMultiplicationOfArrays = new TabulatedFunctionOperationService().multiplication(testArrayFunction, testArrayFunction);
         int i = 0;
@@ -126,8 +126,8 @@ public class TabulatedFunctionOperationServiceTest {
 
     @Test
     public void testDivision() {
-        ArrayTabulatedFunction testArrayFunction = getTestArray();
-        LinkedListTabulatedFunction testListFunction = getTestList();
+        TabulatedFunction testArrayFunction = getTestArray();
+        TabulatedFunction testListFunction = getTestList();
 
         TabulatedFunction testDivisionOfArrays = new TabulatedFunctionOperationService().division(testArrayFunction, testArrayFunction);
         int i = 0;
