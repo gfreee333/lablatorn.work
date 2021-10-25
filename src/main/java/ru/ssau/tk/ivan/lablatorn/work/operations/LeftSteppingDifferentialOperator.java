fde6015d53d34e4816849cc8f1191352bb5f821a
Чухманов Иван Викторович <oollvanolloo@yandex.ru>
2021-10-25 12:49:35 +0400

    исправил очередной косяк во время пушей

--- src/main/java/ru/ssau/tk/ivan/lablatorn/work/operations/LeftSteppingDifferentialOperator.java
+++ src/main/java/ru/ssau/tk/ivan/lablatorn/work/operations/LeftSteppingDifferentialOperator.java
@@ -1,13 +1,8 @@
 package ru.ssau.tk.ivan.lablatorn.work.operations;
 
 import ru.ssau.tk.ivan.lablatorn.work.MathFunction;
-
-<<<<<<< HEAD
-
-public class LeftSteppingDifferentialOperator extends SteppingDifferentialOperator {
-=======
 public class LeftSteppingDifferentialOperator extends SteppingDifferentialOperator{
->>>>>>> origin/main
+
     public LeftSteppingDifferentialOperator(double step) {
         super(step);
     }
