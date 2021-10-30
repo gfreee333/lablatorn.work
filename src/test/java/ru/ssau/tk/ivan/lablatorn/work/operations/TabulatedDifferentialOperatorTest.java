593bcdbedd7a746d713fa265c0592073127a57b4
Чухманов Иван Викторович <oollvanolloo@yandex.ru>
2021-10-30 23:58:02 +0400

    Добавил недостающий тест TabulatedDifferentialOperatorTest

--- src/test/java/ru/ssau/tk/ivan/lablatorn/work/operations/TabulatedDifferentialOperatorTest.java
+++ src/test/java/ru/ssau/tk/ivan/lablatorn/work/operations/TabulatedDifferentialOperatorTest.java
@@ -1,10 +1,29 @@
 package ru.ssau.tk.ivan.lablatorn.work.operations;
 
 import org.testng.annotations.Test;
+import ru.ssau.tk.ivan.lablatorn.work.function.ArrayTabulatedFunction;
+import ru.ssau.tk.ivan.lablatorn.work.function.LinkedListTabulatedFunction;
+import ru.ssau.tk.ivan.lablatorn.work.function.TabulatedFunction;
+import ru.ssau.tk.ivan.lablatorn.work.function.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.ivan.lablatorn.work.function.factory.LinkedListTabulatedFunctionFactory;
+
+import static org.testng.Assert.assertEquals;
 
 public class TabulatedDifferentialOperatorTest {
 
     @Test
     public void testDerive() {
+        TabulatedFunction testList = new LinkedListTabulatedFunction(new double[]{1, 2, 3, 4}, new double[]{1, 4, 9, 16});
+        TabulatedDifferentialOperator differentialListOperator = new TabulatedDifferentialOperator(new LinkedListTabulatedFunctionFactory());
+        testList = differentialListOperator.derive(testList);
+        assertEquals(testList.getY(0), 3, 0.0001);
+        assertEquals(testList.getY(1), 5, 0.0001);
+        assertEquals(testList.getY(3), 7, 0.0001);
+        TabulatedFunction testArray = new ArrayTabulatedFunction(new double[]{1, 2, 3, 4}, new double[]{1, 2, 3, 4});
+        TabulatedDifferentialOperator differentialArrayOperator = new TabulatedDifferentialOperator(new ArrayTabulatedFunctionFactory());
+        testArray = differentialArrayOperator.derive(testArray);
+        assertEquals(testArray.getX(3), 4, 0.0001);
+        assertEquals(testArray.getY(2), 1, 0.0001);
+        assertEquals(testArray.getY(3), 1, 0.0001);
     }
 }
