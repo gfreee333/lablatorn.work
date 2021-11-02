003fecb185633dee74bf79e1dd4b64044e953486
Чухманов Иван Викторович <oollvanolloo@yandex.ru>
2021-11-02 23:50:51 +0400

    Исправил проблему с сериализацией исключений.

--- src/main/java/ru/ssau/tk/ivan/lablatorn/work/exceptions/InconsistentFunctionsException.java
+++ src/main/java/ru/ssau/tk/ivan/lablatorn/work/exceptions/InconsistentFunctionsException.java
@@ -1,6 +1,8 @@
 package ru.ssau.tk.ivan.lablatorn.work.exceptions;
 
 public class InconsistentFunctionsException extends RuntimeException {
+    private static final long serialVersionUID = 559032299456330508L;
+
     public InconsistentFunctionsException() {
     }
 
--- src/main/java/ru/ssau/tk/ivan/lablatorn/work/exceptions/InterpolationException.java
+++ src/main/java/ru/ssau/tk/ivan/lablatorn/work/exceptions/InterpolationException.java
@@ -1,6 +1,8 @@
 package ru.ssau.tk.ivan.lablatorn.work.exceptions;
 
 public class InterpolationException extends RuntimeException {
+    private static final long serialVersionUID = -1840712873933280806L;
+
     public InterpolationException() {
 
     }
